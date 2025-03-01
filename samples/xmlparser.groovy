458752b0814e95feb1e696a5532b5d0aa5dcd949
あーぱつ <ocom344@gmail.com>
2025-03-01 23:16:59 +0900

    Update xmlparser.groovy

--- samples/xmlparser.groovy
+++ samples/xmlparser.groovy
@@ -1,12 +1,18 @@
 import com.sap.gateway.ip.core.customdev.util.Message
-import groovy.xml.XmlParser
+import java.util.HashMap;
+import groovy.xml.*
 
-Message processData(Message message) {
+def Message processData(Message message) {
 	def body = message.getBody(String)
 	def parsedXml = new XmlParser().parseText(body)
 
-	parsedXml.record.each{ data ->
-		
-		
+	parsedXml.book.each{ data ->
+		if (data.auther.text() == 'Ralls, Kim'){
+            data.appendNode('DataFlag', 1)
+        } else {
+            data.appendNode('DataFlag', 0)
+        }
 	}
+    message.setBody(XmlUtil.serialize(parsedXml))
+    return message
 }
