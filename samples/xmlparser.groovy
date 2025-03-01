a526ecbfbde05e8cc973639dc9881b2439d77b25
あーぱつ <ocom344@gmail.com>
2025-03-01 23:00:50 +0900

    Update xmlparser.groovy

--- samples/xmlparser.groovy
+++ samples/xmlparser.groovy
@@ -1,1 +1,12 @@
+import com.sap.gateway.ip.core.customdev.util.Message
+import groovy.xml.XmlParser
 
+Message processData(Message message) {
+	def body = message.getBody(String)
+	def parsedXml = new XmlParser().parseText(body)
+
+	parsedXml.record.each{ data ->
+		
+		
+	}
+}
